00778a3908a7b095cc6ff603df9bb465505d7294
janik <jan.janech@kst.uniza.sk>
2013-01-15 06:37:56 +0000

    using faster reflection to call SortedCollectionDiff.ComputeInternal

--- POCOMerger/diff/collection/SortedCollectionDiff.cs
+++ POCOMerger/diff/collection/SortedCollectionDiff.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using POCOMerger.diff.@base;
@@ -13,6 +14,7 @@ namespace POCOMerger.diff.collection
 	public class SortedCollectionDiff<TType> : IDiffAlgorithm<TType>
 	{
 		private MergerImplementation aMergerImplementation;
+		private Func<TType, TType, IDiff<TType>> aCallComputeInternal;
 
 		public SortedCollectionDiff(MergerImplementation mergerImplementation)
 		{
@@ -23,30 +25,44 @@ namespace POCOMerger.diff.collection
 
 		public IDiff<TType> Compute(TType @base, TType changed)
 		{
-			Type itemType = null;
-
-			foreach (Type @interface in typeof(TType).GetInterfaces())
+			if (this.aCallComputeInternal == null)
 			{
-				if (@interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+				Type itemType = null;
+
+				foreach (Type @interface in typeof(TType).GetInterfaces())
 				{
-					itemType = @interface.GetGenericArguments()[0];
+					if (@interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+					{
+						itemType = @interface.GetGenericArguments()[0];
+					}
 				}
+
+				if (itemType == null)
+					throw new Exception("Cannot compare non-collection type with SortedCollectionDiff");
+
+				MethodInfo computeInternal =
+					this.GetType()
+					    .GetMethod("ComputeInternal", BindingFlags.Instance | BindingFlags.NonPublic)
+					    .MakeGenericMethod(itemType);
+
+				ParameterExpression @baseParameter = Expression.Parameter(typeof(TType), "base");
+				ParameterExpression changedParameter = Expression.Parameter(typeof(TType), "changed");
+
+				Expression<Func<TType, TType, IDiff<TType>>> callComputeExpression =
+					Expression.Lambda<Func<TType, TType, IDiff<TType>>>(
+						Expression.Call(Expression.Constant(this), computeInternal, @baseParameter, changedParameter),
+						@baseParameter, changedParameter
+					);
+
+				this.aCallComputeInternal = callComputeExpression.Compile();
 			}
 
-			if (itemType == null)
-				throw new Exception("Cannot compare non-collection type with SortedCollectionDiff");
-
-			MethodInfo computeInternal =
-				this.GetType()
-				    .GetMethod("ComputeInternal", BindingFlags.Instance | BindingFlags.NonPublic)
-				    .MakeGenericMethod(itemType);
-
-			return (IDiff<TType>) computeInternal.Invoke(this, new object[] { @base, changed });
+			return this.aCallComputeInternal(@base, changed);
 		}
 
 		#endregion
 
-		private IDiff<IEnumerable<TItemType>> ComputeInternal<TItemType>(IEnumerable<TItemType> @base, IEnumerable<TItemType> changed)
+		private IDiff<TType> ComputeInternal<TItemType>(IEnumerable<TItemType> @base, IEnumerable<TItemType> changed)
 		{
 			throw new NotImplementedException();
 		}
