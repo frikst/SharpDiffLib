bf2cb07a8ab895adee1cfb61d6a5c54b865d2a8f
janik <jan.janech@kst.uniza.sk>
2013-01-16 19:45:54 +0000

    tests for dictionary diffs

--- POCOMerger.Test/Diff/DictionaryOfPrimitives.cs
+++ POCOMerger.Test/Diff/DictionaryOfPrimitives.cs
@@ -33,5 +33,106 @@ namespace POCOMerger.Test.Diff
 			Assert.AreEqual(1, ret.Count);
 			Assert.AreEqual(diff, ret.ToString());
 		}
+
+		[TestMethod]
+		public void TwoAdded()
+		{
+			const string diff =
+				"+c:3\r\n" +
+				"+d:4";
+			var @base = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+			var changed = new Dictionary<string, int> { { "a", 1 }, { "b", 2 }, { "c", 3 }, { "d", 4 } };
+
+			var ret = Merger.Instance.Partial.Diff(@base, changed);
+
+			Assert.AreEqual(2, ret.Count);
+			Assert.AreEqual(diff, ret.ToString());
+		}
+
+		[TestMethod]
+		public void TwoRemoved()
+		{
+			const string diff =
+				"-c:3\r\n" +
+				"-d:4";
+			var @base = new Dictionary<string, int> { { "a", 1 }, { "b", 2 }, { "c", 3 }, { "d", 4 } };
+			var changed = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+
+			var ret = Merger.Instance.Partial.Diff(@base, changed);
+
+			Assert.AreEqual(2, ret.Count);
+			Assert.AreEqual(diff, ret.ToString());
+		}
+
+		[TestMethod]
+		public void OneReplaced()
+		{
+			const string diff =
+				"-c:3\r\n" +
+				"+c:4";
+			var @base = new Dictionary<string, int> { { "a", 1 }, { "b", 2 }, { "c", 3 } };
+			var changed = new Dictionary<string, int> { { "a", 1 }, { "b", 2 }, { "c", 4 } };
+
+			var ret = Merger.Instance.Partial.Diff(@base, changed);
+
+			Assert.AreEqual(1, ret.Count);
+			Assert.AreEqual(diff, ret.ToString());
+		}
+
+		[TestMethod]
+		public void AllAdded()
+		{
+			const string diff =
+				"+a:1\r\n" +
+				"+b:2";
+			var @base = new Dictionary<string, int> { };
+			var changed = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+
+			var ret = Merger.Instance.Partial.Diff(@base, changed);
+
+			Assert.AreEqual(2, ret.Count);
+			Assert.AreEqual(diff, ret.ToString());
+		}
+
+		[TestMethod]
+		public void AllRemoved()
+		{
+			const string diff =
+				"-a:1\r\n" +
+				"-b:2";
+			var @base = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+			var changed = new Dictionary<string, int> { };
+
+			var ret = Merger.Instance.Partial.Diff(@base, changed);
+
+			Assert.AreEqual(2, ret.Count);
+			Assert.AreEqual(diff, ret.ToString());
+		}
+
+		[TestMethod]
+		public void UnchangedEmpty()
+		{
+			const string diff = "";
+			var @base = new Dictionary<string, int> { };
+			var changed = new Dictionary<string, int> { };
+
+			var ret = Merger.Instance.Partial.Diff(@base, changed);
+
+			Assert.AreEqual(0, ret.Count);
+			Assert.AreEqual(diff, ret.ToString());
+		}
+
+		[TestMethod]
+		public void UnchangedNonEmpty()
+		{
+			const string diff = "";
+			var @base = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+			var changed = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+
+			var ret = Merger.Instance.Partial.Diff(@base, changed);
+
+			Assert.AreEqual(0, ret.Count);
+			Assert.AreEqual(diff, ret.ToString());
+		}
 	}
 }
